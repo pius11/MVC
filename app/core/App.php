6ff24a90e72fe557a03ff5f20835b10ee36bd514
pius11 <pius17431@gmail.com>
2024-09-30 13:33:26 +0800

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,17 @@
 <?php 
 class App{
     public function __construct(){
-        echo"I WAYAN PIUS WIPRAJANA SAMITA";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl(){
+        if (isset($_GET['url'])) {
+            $url = rtrim( $_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            
+            return $url;
+        }
     }
 }
