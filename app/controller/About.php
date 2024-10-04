fc19965905714f025657378761acc8c93a723a2d
pius11 <pius17431@gmail.com>
2024-10-04 21:52:36 +0800

    modif app.php, and add home and about class

---
+++ app/controller/About.php
@@ -0,0 +1,11 @@
+<?php 
+
+class About{
+
+    public function index($nama='pius', $pekerjaan='mahasiswa'){
+        echo"halo nama saya $nama, saya seorang $pekerjaan";
+    }
+    public function page(){
+        echo"about/page";
+    }
+}
